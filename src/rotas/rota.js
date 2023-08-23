0443c7e04920391e37434ee17dbb7a40646dd7fe
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-08-23 19:12:27 -0300

    Intermediaro para verificacoes de usuario finalizado

--- src/rotas/rota.js
+++ src/rotas/rota.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { cadastrarUsuario } = require('../controladores/controlador_usuarios');
+const { verificaUsuario } = require('../controladores/intermediario_usuarios');
 
 const rota = Router();
 
-rota.post('/usuario', cadastrarUsuario);
+rota.post('/usuario', verificaUsuario, cadastrarUsuario);
 
 module.exports = rota;
